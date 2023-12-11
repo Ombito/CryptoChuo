d459e0f2777ae25dd6f859f128186de78ca3196b
Ombito <alvinombito09@gmail.com>
2023-12-11 22:55:08 +0300

    Style about page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -6,6 +6,17 @@ import { Link } from 'react-router-dom';
 import team2 from "../Assets/team2.png"
 import Navbar from "../Navbar/navbar.jsx";
 import Footer from "../Footer/index.jsx";
+import banner from "../Assets/school.png";
+import ourstory from "../Assets/our-story.png";
+import binance from "../Assets/binance.png";
+import bybit from "../Assets/bybit.png";
+import avalanche from "../Assets/avalanche.png";
+import polygon from "../Assets/polygon.png";
+import opensea from "../Assets/opensea.png";
+import kraken from "../Assets/kraken.png";
+import ledger from "../Assets/ledger.png";
+import gemini from "../Assets/gemini.png";
+
 
 
 const About = () => {
@@ -14,13 +25,19 @@ const About = () => {
         <Navbar /> 
         <div className='aboutus'>
           <div id="about-banner" class="container">
-            <p className='about'>CryptoChuo training provides a strong foundation for executives to understand cryptocurrency <br/>and blockchain. Participants learn the essentials behind blockchain and distributed ledger 
-            <br/>technology(DLT), how cryptocurrency transactions work, Virtual Asset Service Provider(VASP) <br/>typologies, industry needs, and the role blockchain analytics plays in the crypto ecosystem.
-            <br/>CryptoChuo graduates make more informed decisions around cryptocurrency, whether it brings 
-            <br/>drafting regulations, creating compliance policies, and/or overseeing cryptocurrency-enabled <br/>financial crime investigations.</p>
+            <div> 
+              <p className='about'>CryptoChuo training provides a strong foundation for executives to understand cryptocurrency and blockchain. Participants learn the essentials behind blockchain and distributed ledger 
+              technology(DLT), how cryptocurrency transactions work, Virtual Asset Service Provider(VASP) typologies, industry needs, and the role blockchain analytics plays in the crypto ecosystem.
+              CryptoChuo graduates make more informed decisions around cryptocurrency, whether it brings 
+              drafting regulations, creating compliance policies, and/or overseeing cryptocurrency-enabled financial crime investigations.
+              </p>
+            </div>
+            <div>
+              <img src={banner} alt="" />
+            </div>
           </div>
           <div id="our-story" class="container">
-            <img src={team2} alt="" height="400" width="400"/>
+            <img src={ourstory} alt="" height="400" width="400"/>
             <div>
               <h2>Our Story</h2>
               <h1>We accelerate Your growth</h1>
@@ -30,6 +47,22 @@ const About = () => {
 
                 Our teams of researchers, instructors, and subject matter experts are real-world industry professionals with immense knowledge and significant experience.
               </p>
+            </div>
+          </div>
+          <div id="partner-container">
+            <div class="container" id="partners">
+              <div className="hero-partners">
+                <h4>Our Partners</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, mollis ultricies aliquet aptent augue blandit, urna diam sociosqu  est suspendisse fusce turpis montes quam eget sollicitudin</p>
+              </div>
+              <div id="logo-div">
+                <img src={binance} alt="" height="150" width="230" />
+                <img src={opensea} alt="" height="100" width="150" />
+                <img src={avalanche} alt="" height="90" width="100" />
+                <img src={polygon} alt="" height="90" width="100" />
+                <img src={ledger} alt="" height="60" width="150" />
+                <img src={gemini} alt="" height="70" width="150" />
+              </div>
             </div>
           </div>
           <div >
