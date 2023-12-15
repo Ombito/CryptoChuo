d5a1d74675a693e86fd7f4b4e74c22ff5ff83bad
Ombito <alvinombito09@gmail.com>
2023-12-15 23:50:37 +0300

    Fix navbar responsiveness

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -23,30 +23,33 @@ const Navbar = () => {
             <div className="navigation-container">
                 <button onClick={toggle} className='navbar-icons'>{active ? <FaTimes id="close"/> : <FaBars />}</button>
                 <div id="navlinks" className={active ? 'active' : '' } onClick={closeMenu}>
-                    <NavLink to='/' id='home'>
-                        Home
-                    </NavLink>
-                    <NavLink to='/about' id='about'>
-                        About Us
-                    </NavLink>
-                    <NavLink to='/courses' id='courses'>
-                        Courses
-                    </NavLink>
-                    <NavLink to='/markets' id='markets'>
-                        Markets
-                    </NavLink>
-                    <NavLink to='/shop' id='shop'>
-                        Shop
-                    </NavLink>
+                    <div className="navbar-menu">
+                        <NavLink to='/' id='home'>
+                            Home
+                        </NavLink>
+                        <NavLink to='/about' id='about'>
+                            About Us
+                        </NavLink>
+                        <NavLink to='/courses' id='courses'>
+                            Courses
+                        </NavLink>
+                        <NavLink to='/markets' id='markets'>
+                            Markets
+                        </NavLink>
+                        <NavLink to='/shop' id='shop'>
+                            Shop
+                        </NavLink>
+                    </div>
+                    <div className="auth-button">
+                        <Link to="/cart">
+                            <FaShoppingCart id="cart-icon"/>
+                        </Link>
+                        <Link to="/login">
+                            <button id='button' type='submit'>Sign in</button>
+                        </Link>
+                    </div>
                 </div>
-                <div className="auth-button">
-                    <Link to="/cart">
-                        <FaShoppingCart id="cart-icon"/>
-                    </Link>
-                    <Link to="/login">
-                        <button id='button' type='submit'>Sign in</button>
-                    </Link>
-                </div>
+                
             </div>
         </div>
     );
