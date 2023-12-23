b41a3c6546183f5c466be1cc6a7a2e768011a042
Ombito <alvinombito09@gmail.com>
2023-12-23 19:15:39 +0300

    Feat: add search

--- src/components/Markets/index.jsx
+++ src/components/Markets/index.jsx
@@ -4,17 +4,18 @@ import { NavLink } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Navbar from '../Navbar/navbar.jsx';
 import Footer from '../Footer/index.jsx';
+import { Line } from 'react-chartjs-2';
+
 
 const Markets = () => {
   const [loading, setLoading] = useState(true);
   const [markets, setMarkets] = useState([]);
-  
+  const [searchTerm, setSearchTerm] = useState('');
   
   useEffect(() => {
 
     const apiUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d%2C30d&locale=en`;
     
-
     fetch(apiUrl)
       .then((response) => {
         if (!response.ok) {
@@ -33,64 +34,74 @@ const Markets = () => {
       });
   }, []);
 
+  const filteredMarkets = markets.filter((market) =>
+  market.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+  market.symbol.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
   return (
     <div>
-    <div className='navbar'>       
-      <Navbar />
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div id="market">
+        <h3>Today's Cryptocurrency Prices by Market Cap</h3>
+        <input type="text" value={searchTerm} onChange={handleSearch}/>
+      </div>
+      <div>
+        {loading || markets.length === 0 ? (
+          <p className="loading">Fetching market data...</p>
+        ) : (
+          <table className="crypto-table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Coin</th>
+                <th>Price</th>
+                <th>1h</th>
+                <th>24h</th>
+                <th>7d</th>
+                <th>Total Volume</th>
+                <th>Market Cap</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredMarkets.map((i) => (
+                <tr key={i.id}>
+                  <td>{i.market_cap_rank}</td>
+                  <td>
+                    <div className="coin-info">
+                      <img src={i.image} id="coin_img" alt="Coin Image" />
+                      <div className="coin-name">
+                        <p>{i.name}</p>
+                        <p> {i.symbol.toUpperCase()}</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>${i.current_price.toFixed(2)}</td>
+                  <td className={i.price_change_percentage_1h_in_currency < 0 ? 'negative-change' : 'positive-change'}>
+                    {i.price_change_percentage_1h_in_currency.toFixed(2)}%
+                  </td>
+                  <td className={i.price_change_percentage_24h_in_currency < 0 ? 'negative-change' : 'positive-change'}>
+                    {i.price_change_percentage_24h_in_currency.toFixed(2)}%
+                  </td>
+                  <td className={i.price_change_percentage_7d_in_currency < 0 ? 'negative-change' : 'positive-change'}>
+                    {i.price_change_percentage_7d_in_currency.toFixed(2)}%
+                  </td>
+                  <td>${i.total_volume}</td>
+                  <td>${i.market_cap}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+      <Footer />
     </div>
-    <div>
-    <table className="crypto-table">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Coin</th>
-          <th>Price</th>
-          <th>1h</th>
-          <th>24h</th>
-          <th>Total Volume</th>
-          <th>Market Cap</th>
-          <th>Last 7 Days</th>
-        </tr>
-      </thead>
-      <tbody>
-        {markets.map((i) => (
-          <tr key={i.id}>
-            <td>{i.market_cap_rank}</td>
-            <td>
-              <div className="coin-info">
-                <img src={i.image} id="coin_img" alt="Coin Image" />
-                <div className="coin-name">
-                  <p>{i.name}</p>
-                  <p>{i.symbol.toUpperCase()}</p>
-                </div>
-              </div>
-            </td>
-            <td>${i.current_price.toFixed(2)}</td>
-            <td className={i.price_change_percentage_1h_in_currency < 0 ? 'negative-change' : 'positive-change'}>{i.price_change_percentage_1h_in_currency.toFixed(2)}%
-            </td>
-            <td className={i.price_change_percentage_24h_in_currency < 0 ? 'negative-change' : 'positive-change'}>{i.price_change_percentage_24h_in_currency.toFixed(2)}%
-            </td>
-            <td>${i.total_volume}</td>
-            <td>${i.market_cap}</td>
-            <td></td>
-          </tr>
-        ))}
-  </tbody>
-</table>
-
-    </div>
-    <Footer />
-    
-          
-</div>
-
-
-
-
-
-  
   );
 };
-  
+
 export default Markets;
