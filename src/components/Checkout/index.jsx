85e91127191286976e1b0d44474a68964964327f
Ombito <alvinombito09@gmail.com>
2024-01-02 23:25:13 +0300

    Style checkout

--- src/components/Checkout/index.jsx
+++ src/components/Checkout/index.jsx
@@ -11,35 +11,29 @@ const Checkout = () => {
         <div id="checkout" class="container">
             <div>
                 <h2>Secure Checkout</h2>
-                <div>
+                <div className="checkout-hero">
                     <div>
-                        <p><FaCheck /></p>
-                        <p>1. Shopping cart</p>
+                        <p id="cart-checkout"className="checkout-head"><FaCheck className="check-icon" /></p>
+                        <p>Shopping Cart</p>
                     </div>
-                    <div></div>
+                    <div className='line'></div>
                     <div>
-                        <p>2</p>
-                        <p>2. Delivery Address</p>
+                        <p className="checkout-head">2</p>
+                        <p>Delivery Address</p>
                     </div>
-                    <div></div>
+                    <div className='line'></div>
                     <div>
-                        <p>3. Billing & payment</p>
+                        <p className="checkout-head">3</p>
+                        <p>Billing & Payment</p>
                     </div>
                 </div>
-                <div>
+                <div id="order-details">
                     <form id="checkout-form">
-                        <div id="name-div" className="input-div">
-                            <div>
-                                <label>First Name</label>
-                                <input type="text" />
-                            </div>
-                            <div>
-                                <label>Last Name</label>
-                                <input type="text" />
-                            </div>
-                        </div>
+                        <h3>Delivery Address</h3>
                         <label>Full Address</label>
                         <textarea></textarea>
+                        <label>Phone Number</label>
+                        <input type="number" placeholder='Enter phone number'required />
                         <div className="input-div">
                             <div>
                                 <label>City</label>
@@ -56,6 +50,54 @@ const Checkout = () => {
                         </div>
                     </form>
                     <div>
+                        <h3>Order Summary</h3>
+                        <div>
+                            <div>
+                                <input type='text' placeholder='Gift card or discount code' />
+                                <button>Apply</button>
+                            </div>
+                            <div>
+                                <h5>Sub Total</h5>
+                                <h4>$</h4>
+                            </div>
+                            <div>
+                                <h5>Delivery Charges</h5>
+                                <h4>$</h4>
+                            </div>
+                            <div></div>
+                            <div>
+                                <h5>Total</h5>
+                                <h4>$</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h3>Choose your payment method</h3>
+                </div>
+                <div>
+                    <h3>Billing Details</h3>
+                    <div>
+                        <label>Debit/Credit Card</label>
+                        <label>Card Number</label>
+                        <input type="text" placeholder='0000 0000 0000 0000' required />
+                        <label>Name on Card</label>
+                        <input type="text" placeholder='Enter Name' required />
+                        <div>
+                            <div>
+                                <label>Expiration Date</label>
+                                <input type="text" placeholder="MM/YYYY" required />
+                            </div>
+                            <div>
+                                <label>Security Code</label>
+                                <input type="text" placeholder="CVC" required />
+                            </div>
+                        </div>
+                        <div>
+                            <input type="checkbox" required/>
+                            <label>I agree to the Terms and Conditions</label>
+                        </div>
+                        <button>Purchase Now</button>
                     </div>
                 </div>
             </div>
