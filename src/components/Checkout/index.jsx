8cb63616398151d3256fc680231efe0185da056e
Ombito <alvinombito09@gmail.com>
2024-01-08 18:45:36 +0300

    Fix checkout

--- src/components/Checkout/index.jsx
+++ src/components/Checkout/index.jsx
@@ -36,23 +36,40 @@ const Checkout = () => {
                 <div id="order-details">
                     <form id="checkout-form">
                         <h3>Delivery Address</h3>
-                        <label>Full Address</label>
-                        <textarea></textarea>
+                        <div className="input-div">
+                            <div>
+                                <label>First name</label>
+                                <input type="text" placeholder='Enter first name' required/>
+                            </div>
+                            <div>
+                                <label>Last name</label>
+                                <input type="text" placeholder='Enter last name' required/>
+                            </div>
+                        </div>
                         <label>Phone Number</label>
-                        <input type="number" placeholder='Enter phone number'required />
+                        <input type="number" placeholder='Enter phone number' required />
+                        <label>Street Address</label>
+                        <input type="text" placeholder='Enter Street and Number' required />
                         <div className="input-div">
                             <div>
                                 <label>City</label>
-                                <input type="text" />
+                                <input type="text" placeholder='City'/>
                             </div>
                             <div>
                                 <label>State</label>
-                                <input type="text" />
+                                <input type="text" placeholder='State'/>
                             </div>
                             <div>
                                 <label>Zip Code</label>
-                                <input type="text" />
+                                <input type="text" placeholder='00-000' required/>
                             </div>
+                        </div>
+                        <div>
+                            <h3>Choose your payment method</h3>
+                        </div>
+                        <div>
+                            <h3>Payment Details</h3>
+                            {!paymentComplete && <Paypal onPaymentSuccess={handlePaymentSuccess} />}
                         </div>
                     </form>
                     <div>
@@ -72,18 +89,27 @@ const Checkout = () => {
                             </div>
                             <div></div>
                             <div>
-                                <h5>Order Total</h5>
+                                <h5>Grand Total</h5>
                                 <h4>$</h4>
                             </div>
                         </div>
+                        <div>
+                            <div>
+                                <h6>Need Help? Contact us!</h6>
+                                <p>Please call</p>
+                                <h5>+254722222222</h5>
+                                <p>Monday-Friday 8:00-20:00</p>
+                                <p>Saturday 8:00-16:00</p>
+                            </div>
+                            <div>
+                                <h6>Delivery Methods</h6>
+                                <div>
+                                    <img src='' alt="FedEx" />
+                                    <img src='' alt="DHL" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <h3>Choose your payment method</h3>
-                </div>
-                <div>
-                    <h3>Payment Details</h3>
-                    {!paymentComplete && <Paypal onPaymentSuccess={handlePaymentSuccess} />}
                 </div>
             </div>
         </div>
